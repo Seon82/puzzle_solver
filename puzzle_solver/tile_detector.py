1bd9fe6663780b38e8ed46459c1e7aeb1dfd04e9
Dylan Sechet <dylan.sechet@student-cs.fr>
2023-04-17 09:01:50 +0200

    final code

--- puzzle_solver/tile_detector.py
+++ puzzle_solver/tile_detector.py
@@ -2,9 +2,12 @@ import cv2
 
 
 def detect_tiles(img):
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # blurred = cv2.GaussianBlur(gray, (5, 5), 1)
+    # ret, thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 1)
-    ret, thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(blurred, 250, 255, cv2.THRESH_BINARY)
     (num_labels, labels, stats, centroids) = cv2.connectedComponentsWithStats(255 - thresh, 8, cv2.CV_32S)
     boxes = []
     for i in range(0, num_labels):
